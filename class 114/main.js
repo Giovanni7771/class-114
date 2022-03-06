e59c70d22865be2e379ecd3cf49daac2ef893d73
Giovanni7771 <85034061+Giovanni7771@users.noreply.github.com>
2022-03-06 09:59:17 -0800

    Add files via upload

---
+++ class 114/main.js
@@ -0,0 +1,15 @@
+function preload() {
+
+}
+
+function setup(){
+    canvas = creatCanvas(300, 300);
+    canvas.center();
+}
+
+function draw(){
+
+}
+function take_snapshot(){
+    save('myFlterImage.png');
+}
